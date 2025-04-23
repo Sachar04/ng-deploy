ca683a1b7e096125db723aa2def1ebc62210e7bb
Sachar04 <97813614+Sachar04@users.noreply.github.com>
2025-04-23 16:19:52 +0200

    put img inside the link anchor

--- src/app/housing-location/housing-location.component.ts
+++ src/app/housing-location/housing-location.component.ts
@@ -9,8 +9,9 @@ import { RouterModule } from '@angular/router';
   imports: [CommonModule, RouterModule],
   template: `
     <section class="listing">
-      <a [routerLink]="['/details', housingLocation.id]"></a>
-      <img [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}" class="listing-photo">
+      <a [routerLink]="['/details', housingLocation.id]">
+        <img [src]="housingLocation.photo" alt="Exterior photo of {{housingLocation.name}}" class="listing-photo">
+      </a>
       <h2 class="listing-heading">{{ housingLocation.name }}</h2>
       <p class="listing-location">
         {{ housingLocation.city }}, {{housingLocation.state}}
