95f6b7c1af4bb9987e16d7924d654a2a5fbf281b
Sachar04 <97813614+Sachar04@users.noreply.github.com>
2025-04-23 14:32:26 +0200

    FINALLY FIXED ASSETS DISPLAY, it works now as intended!

--- src/app/staticHousingLocation.ts
+++ src/app/staticHousingLocation.ts
@@ -6,7 +6,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Acme Fresh Start Housing",
         "city": "Chicago",
         "state": "IL",
-        "photo": "/assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
+        "photo": "assets/bernard-hermant-CLKGGwIBTaY-unsplash.jpg",
         "availableUnits": 4,
         "wifi": true,
         "laundry": true
@@ -16,7 +16,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "A113 Transitional Housing",
         "city": "Santa Monica",
         "state": "CA",
-        "photo": "/assets/brandon-griggs-wR11KBaB86U-unsplash.jpg",
+        "photo": "assets/brandon-griggs-wR11KBaB86U-unsplash.jpg",
         "availableUnits": 0,
         "wifi": false,
         "laundry": true
@@ -26,7 +26,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Warm Beds Housing Support",
         "city": "Juneau",
         "state": "AK",
-        "photo": "/assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
+        "photo": "assets/i-do-nothing-but-love-lAyXdl1-Wmc-unsplash.jpg",
         "availableUnits": 1,
         "wifi": false,
         "laundry": false
@@ -36,7 +36,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Homesteady Housing",
         "city": "Chicago",
         "state": "IL",
-        "photo": "/assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg",
+        "photo": "assets/ian-macdonald-W8z6aiwfi1E-unsplash.jpg",
         "availableUnits": 1,
         "wifi": true,
         "laundry": false
@@ -46,7 +46,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Happy Homes Group",
         "city": "Gary",
         "state": "IN",
-        "photo": "/assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg",
+        "photo": "assets/krzysztof-hepner-978RAXoXnH4-unsplash.jpg",
         "availableUnits": 1,
         "wifi": true,
         "laundry": false
@@ -56,7 +56,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Hopeful Apartment Group",
         "city": "Oakland",
         "state": "CA",
-        "photo": "/assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg",
+        "photo": "assets/r-architecture-JvQ0Q5IkeMM-unsplash.jpg",
         "availableUnits": 2,
         "wifi": true,
         "laundry": true
@@ -66,7 +66,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Seriously Safe Towns",
         "city": "Oakland",
         "state": "CA",
-        "photo": "/assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg",
+        "photo": "assets/phil-hearing-IYfp2Ixe9nM-unsplash.jpg",
         "availableUnits": 5,
         "wifi": true,
         "laundry": true
@@ -76,7 +76,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Hopeful Housing Solutions",
         "city": "Oakland",
         "state": "CA",
-        "photo": "/assets/r-architecture-GGupkreKwxA-unsplash.jpg",
+        "photo": "assets/r-architecture-GGupkreKwxA-unsplash.jpg",
         "availableUnits": 2,
         "wifi": true,
         "laundry": true
@@ -86,7 +86,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Seriously Safe Towns",
         "city": "Oakland",
         "state": "CA",
-        "photo": "/assets/saru-robert-9rP3mxf8qWI-unsplash.jpg",
+        "photo": "assets/saru-robert-9rP3mxf8qWI-unsplash.jpg",
         "availableUnits": 10,
         "wifi": false,
         "laundry": false
@@ -96,7 +96,7 @@ export const housingLocations : HousingLocation[] = [
         "name": "Capital Safe Towns",
         "city": "Portland",
         "state": "OR",
-        "photo": "/assets/webaliser-_TPTXZd9mOo-unsplash.jpg",
+        "photo": "assets/webaliser-_TPTXZd9mOo-unsplash.jpg",
         "availableUnits": 6,
         "wifi": true,
         "laundry": true
